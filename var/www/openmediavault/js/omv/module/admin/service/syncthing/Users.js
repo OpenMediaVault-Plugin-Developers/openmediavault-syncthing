e3abf87b17ddfbfc6b4dd6d60759d0203192e1b8
Aaron Murray <plugins@omv-extras.org>
2017-03-11 15:39:43 -0600

    update locales

--- var/www/openmediavault/js/omv/module/admin/service/syncthing/Users.js
+++ var/www/openmediavault/js/omv/module/admin/service/syncthing/Users.js
@@ -134,6 +134,15 @@ Ext.define("OMV.module.admin.service.syncthing.User", {
                 ptype: "fieldinfo",
                 text: _("Limit receive speed. Value is Kb/s. 0 to disable.")
             }]
+        },{
+            xtype      : "checkbox",
+            name       : "inotify",
+            fieldLabel : _("Inotify"),
+            checked    : true,
+            plugins: [{
+                ptype: "fieldinfo",
+                text: _("Enable syncthing inotify.  Use inotify to detect changes almost instantly.")
+            }]
         }];
     }
 });
